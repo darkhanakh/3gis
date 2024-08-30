0e612f4f6b81d266126577c3e78c2999841704f6
Darkhan <darkhanahmetov228@gmail.com>
2024-08-31 00:43:38 +0500

    feat: translated login to russian, add link to register

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -4,19 +4,21 @@ import {
   CardTitle,
   CardDescription,
   CardContent,
+  CardFooter,
 } from '@/components/ui/card';
 import { Label } from '@/components/ui/label';
 import { Input } from '@/components/ui/input';
 import { Button } from '@/components/ui/button';
+import Link from 'next/link';
 
 export default function LoginPage() {
   return (
-    <div className="flex items-center justify-center min-h-screen ">
+    <div className="flex items-center justify-center min-h-screen">
       <Card className="mx-auto max-w-sm">
         <CardHeader className="space-y-1">
-          <CardTitle className="text-2xl font-bold">Login</CardTitle>
+          <CardTitle className="text-2xl font-bold">Войти</CardTitle>
           <CardDescription>
-            Enter your email and password to login to your account
+            Введите ваш email и пароль для входа в аккаунт
           </CardDescription>
         </CardHeader>
         <CardContent>
@@ -31,14 +33,22 @@ export default function LoginPage() {
               />
             </div>
             <div className="space-y-2">
-              <Label htmlFor="password">Password</Label>
+              <Label htmlFor="password">Пароль</Label>
               <Input id="password" type="password" required />
             </div>
             <Button type="submit" className="w-full">
-              Login
+              Войти в аккаунт
             </Button>
           </div>
         </CardContent>
+        <CardFooter>
+          <div className="text-sm text-center w-full">
+            Нет аккаунта?{' '}
+            <Link href="/register" className="text-primary hover:underline">
+              Создать аккаунт
+            </Link>
+          </div>
+        </CardFooter>
       </Card>
     </div>
   );
